b3783a43bb6746ba353988cff671187d1aafe8d6
Bryan C <69018393+brytich@users.noreply.github.com>
2022-02-28 14:00:58 +0100

    Update api.js

--- ApplisMovie/src/services/api.js
+++ ApplisMovie/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export const key = 'e5f25f5d7084683d80504b58889f704e'
-
+//Lien de L API externe complete , ESP trouvé seulement
 const api = axios.create({
     baseURL: 'https://api.themoviedb.org/3'
 })
